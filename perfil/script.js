9b9ed70b1f4cb9d2618cfec52794eb7f6d1a81e4
Pedro Henrique <pedrohenriquemartinscorrea@gmail.com>
2025-02-06 18:41:32 -0300

    MyProfile Page

--- perfil/script.js
+++ perfil/script.js
@@ -8,6 +8,30 @@ setInterval(function () {
 
 function resumo() {
   document.getElementById('lista_container_resumo').style.display = 'flex';
-  document.getElementById('lista__itens').style.display = 'block'
+  document.getElementById('lista__itens-resumo').style.display = 'block'
 }
 
+function habilidades() {
+    document.getElementById('lista_container_habilidades').style.display = 'flex';
+  document.getElementById('lista__itens-habilidades').style.display = 'block';
+}
+
+function experiencias() {
+    document.getElementById('lista_container_experiencias').style.display = 'flex';
+  document.getElementById('lista__itens-experiencias').style.display = 'block';
+}
+
+function formacao() {
+    document.getElementById('lista_container_formacao').style.display = 'flex';
+  document.getElementById('lista__itens-formacao').style.display = 'block';
+}
+
+function cursos() {
+    document.getElementById('lista_container_cursos').style.display = 'flex';
+  document.getElementById('lista__itens-cursos').style.display = 'block';
+}
+
+function certificados() {
+    document.getElementById('lista_container_certificados').style.display = 'flex';
+  document.getElementById('lista__itens-certificados').style.display = 'block';
+}
